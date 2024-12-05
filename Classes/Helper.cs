f1e4c107c59181ab99d4801ad229ec20a8e4cb16
mornangann <128682724+mornangann@users.noreply.github.com>
2024-12-05 15:25:44 +0300

    Add files via upload

---
+++ Classes/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ОООТехносервис.Classes
+{
+    //Класс с глобальными величинами для всего проекта
+    internal class Helper
+    {
+        //Связка с БД
+        public static model.DBConnectRequest DBRequest { get; set; }
+
+        //Пользователь, вошедший в систему
+        public static model.User user;
+    }
+}
